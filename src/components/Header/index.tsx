7969784658849fcde27abb89e8a160e7acdc442f
jbiers <juliabiersuriano@gmail.com>
2022-03-24 11:19:56 -0400

    lift state up

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,28 +4,19 @@ import logoImg from '../../assets/logo.svg'
 
 import { Container, Content } from './styles'
 
-export function Header() {
-    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+interface HeaderProps {
+    onOpenNewTransactionModal: () => void;
+}
 
-    function handleOpenNewTransactionModal() {
-        setIsNewTransactionModalOpen(true)
-    }
-
-    function handleCloseNewTransactionModal() {
-        setIsNewTransactionModalOpen(false)
-    }
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
 
     return (
         <Container>
             <Content>
                 <img src={logoImg} alt="dt money" />
-                <button type="button" onClick={handleOpenNewTransactionModal}>
+                <button type="button" onClick={onOpenNewTransactionModal}>
                     New Transaction
                 </button>
-
-                <Modal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}>
-                    <h2>Cadastrar transação</h2>
-                </Modal>
             </Content>
         </Container>
     )
